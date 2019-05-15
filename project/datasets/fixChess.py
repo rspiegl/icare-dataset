d163f763b42ba40c297b12a206272bf39482d3cf
Rick Spiegl <rick.spiegl@student.uibk.ac.at>
2019-05-15 12:53:33 +0200

    add python script to fix chess labeling

---
+++ project/datasets/fixChess.py
@@ -0,0 +1,17 @@
+def relabel(path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+
+    split = [[line.split()[0], int(line.split()[1])] for line in lines]
+    with open(path, 'w') as file:
+        for item in split:
+            file.write(item[0] + ' ' + str((1 - item[1])) + '\n')
+
+
+if __name__ == '__main__':
+    datasets = ['camerarot_diff1/labels.txt',
+                'camerarot_diff5/labels.txt',
+                'camerarot_diff10/labels.txt',]
+
+    for dataset in datasets:
+        relabel(dataset)
