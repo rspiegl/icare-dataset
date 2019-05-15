d7432ec8eeec66c28745fd5c6ff25ce9b9e9c18d
Rick Spiegl <rick.spiegl@student.uibk.ac.at>
2019-05-15 12:15:47 +0200

    add DatasetLoader check for files

--- project/Utilities.py
+++ project/Utilities.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 import sys
 import time
@@ -9,7 +10,9 @@ class DatasetLoader:
     @staticmethod
     def load_problem(path=DATASETS_PATH, shuffle=True):
         try:
-            file = open(path + 'labels.txt', 'r')
+            with open(path + 'labels.txt', 'r') as file:
+                lines = file.readlines()
+
         except FileNotFoundError as fnfe:
             print(path + 'labels.txt does not exist')
             raise fnfe
@@ -17,13 +20,25 @@ class DatasetLoader:
             print("Unexpected error:", sys.exc_info()[0])
             raise exc
 
-        lines = file.readlines()
-        images = [[path + item.split()[0], int(item.split()[1])] for item in lines]
+        splitted_lines = [[path + line.split()[0], int(line.split()[1])] for line in lines]
+
+        # check if all files are present
+        not_existing = []
+        for index, line in enumerate(splitted_lines):
+            if not os.path.isfile(line[0]):
+                not_existing.append(index)
+
+        if not_existing:
+            print("Files that aren't available:")
+            for index in not_existing:
+                print(splitted_lines[index][0])
+
+            splitted_lines = [line for index, line in enumerate(splitted_lines) if index not in not_existing]
 
         if shuffle:
-            random.shuffle(images)
+            random.shuffle(splitted_lines)
 
-        return images
+        return splitted_lines
 
     @staticmethod
     def save_to_file(data):
