7008c9268eeec66b2f7714dac0a709697fca1b14
Rick Spiegl <rick.spiegl@student.uibk.ac.at>
2019-07-14 19:06:16 +0200

    add option to load evaluation from files

--- project/Evaluation.py
+++ project/Evaluation.py
@@ -1,16 +1,18 @@
-import ast
 import statistics
 import time
 
+CUSTOM_EVAL_NAN = {'nan': float('nan')}
 
 class Evaluation:
 
-    def __init__(self, tester_data, tracker_data):
+    def __init__(self, tester_data, tracker_data, data=None):
         self.tester_data = tester_data
         self.tracker_data = tracker_data
-        self.data = {}
 
-        self.evaluate()
+        if data:
+            self.data = data
+        else:
+            self.evaluate()
 
     def evaluate(self):
         self.data["p"] = sum(i[0].count(1) for i in self.tester_data)
@@ -60,6 +62,26 @@ class Evaluation:
             for data in self.tracker_data:
                 file.write(str(data) + '\n')
 
-    def load_from_file(self, path):
+    @staticmethod
+    def _load_from_file(path):
         with open(path, 'r') as file:
-            self.data = ast.literal_eval(file.read())
+            data = eval(file.read(), CUSTOM_EVAL_NAN)
+
+        return data
+
+    @staticmethod
+    def _load_tracker_data_from_file(path):
+        tracker_data = []
+
+        with open(path, 'r') as file:
+            for line in [s.strip() for s in file.readlines()]:
+                tracker_data.append(eval(line, CUSTOM_EVAL_NAN))
+
+        return tracker_data
+
+    @staticmethod
+    def create_from_files(data, tracker):
+        data = Evaluation._load_from_file(data)
+        tracker_data = Evaluation._load_tracker_data_from_file(tracker)
+
+        return Evaluation(data["tester_data"], tracker_data, data)
