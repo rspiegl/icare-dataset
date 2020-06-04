4e2cc5f6e441bf6329cd976add50849d9e10ca64
Rick Spiegl <rick.spiegl@student.uibk.ac.at>
2020-06-04 17:07:06 +0200

    remove unnecessary code

--- project/processing/Evaluate.py
+++ project/processing/Evaluate.py
@@ -12,10 +12,10 @@ from matplotlib.figure import Figure
 from processing import Prepare, InteractiveSelectors
 from processing.pygazeanalyser import detectors, gazeplotter
 
-DATASETS = ['sr', 'svrt1', 'random_board_images_big_diff5',
+DATASETS = ('sr', 'svrt1', 'random_board_images_big_diff5',
             'random_board_images_big_diff1', 'sd', 'svrt19', 'camerarot_diff5',
             'camerarot_diff1', 'svrt20', 'svrt21', 'rot_images_diff5',
-            'rot_images_diff1']
+            'rot_images_diff1')
 GEOMETRY = (703, 54, 512, 512)
 PATH = "processing/prepared/{}/{}/"
 IMAGE_NAME_REGEX = r'[^\/]+\/\d+_([^\/]+)\.'
@@ -106,8 +106,8 @@ def offset_calibration(image_df: pd.DataFrame, calibration_df: pd.DataFrame):
 
     xybins = 40
     bin_middle = GEOMETRY[2] / xybins / 2
-    H, xedges, yedges = np.histogram2d(xy_cali[:, 0], xy_cali[:, 1], bins=xybins, range=RANGE_PIC)
-    x_cent, y_cent = np.unravel_index(H.argmax(), H.shape)
+    h, xedges, yedges = np.histogram2d(xy_cali[:, 0], xy_cali[:, 1], bins=xybins, range=RANGE_PIC)
+    x_cent, y_cent = np.unravel_index(h.argmax(), h.shape)
     x_offset = xedges[x_cent] + bin_middle - (RANGE_PIC[0][1] // 2)
     y_offset = yedges[y_cent] + bin_middle - (RANGE_PIC[1][1] // 2)
 
@@ -121,65 +121,7 @@ def offset_calibration_default(image_df: pd.DataFrame):
     return np.asarray(new, dtype=np.int)
 
 
-def draw_plots(trim: np.ndarray, cali_trim: np.ndarray, image_path: str, save_path: str, trim_fix: dict,
-               trim_sac: list, good_path: str = None):
-    img = Image.open(image_path)
-    img = img.resize(PLOT_SIZE)
-    figure = Figure(figsize=(18, 11.5), dpi=DPI)
-    canvas = FigureCanvas(figure)
-    axes = figure.subplots(ncols=3, nrows=2)
-    axes[0][0].set_title('Original')
-    axes[0][0].imshow(img, zorder=1)
-    if trim is not None:
-        axes[0][1].set_title('Uncalibrated')
-        axes[0][1].hist2d(trim[0], trim[1], bins=BINS, range=RANGE_PIC, alpha=0.6, zorder=2, cmin=0.01)
-        axes[0][1].invert_yaxis()
-        axes[0][1].imshow(img, zorder=1)
-    else:
-        axes[0][1].axis('off')
-    if cali_trim is not None:
-        axes[0][2].set_title('Calibrated')
-        axes[0][2].hist2d(cali_trim[0], cali_trim[1], bins=BINS, range=RANGE_PIC, alpha=0.6, zorder=2, cmin=0.01)
-        axes[0][2].invert_yaxis()
-        axes[0][2].imshow(img, zorder=1)
-    else:
-        axes[0][2].axis('off')
-    if trim_fix:
-        siz = 3 * (trim_fix['dur'] / 30)
-        axes[1][0].set_title('Fixations')
-        axes[1][0].imshow(img, zorder=1)
-        axes[1][0].scatter(trim_fix['x'], trim_fix['y'], s=siz, marker='o', color='g', alpha=0.7, zorder=2)
-
-        axes[1][1].set_title('Scanpath')
-        axes[1][1].imshow(img, zorder=1)
-        for i in range(len(trim_fix['x'])):
-            axes[1][1].annotate(str(i), (trim_fix['x'][i], trim_fix['y'][i]), zorder=4, alpha=1,
-                                horizontalalignment='center', verticalalignment='center',
-                                multialignment='center', color='g')
-
-        if trim_sac:
-            axes[1][2].set_title('Scanpath zoomed with saccades')
-            axes[1][2].scatter(trim_fix['x'], trim_fix['y'], s=siz, marker='o', color='g', alpha=0.2, zorder=2)
-            for i in range(len(trim_fix['x'])):
-                axes[1][2].annotate(str(i), (trim_fix['x'][i], trim_fix['y'][i]), zorder=4, alpha=1,
-                                    horizontalalignment='center', verticalalignment='center',
-                                    multialignment='center')
-            for st, et, dur, sx, sy, ex, ey in trim_sac:
-                axes[1][2].arrow(sx, sy, ex - sx, ey - sy, alpha=0.7, zorder=3, fill=True, shape='full', width=10,
-                                 head_width=20, head_starts_at_zero=False, overhang=0)
-            axes[1][2].invert_yaxis()
-        else:
-            axes[1][2].axis('off')
-    else:
-        axes[1][0].axis('off')
-        axes[1][1].axis('off')
-
-    figure.canvas.print_png(save_path)
-    figure.canvas.print_png(good_path)
-    figure.clf()
-
-
-def draw_plots_2(trim_fix: dict, cali_trim_fix: dict, stat_string:str, image_path: str, save_path: str,):
+def draw_plots(trim_fix: dict, cali_trim_fix: dict, stat_string: str, image_path: str, save_path: str, ):
     img = Image.open(image_path)
     img = img.resize(PLOT_SIZE)
     figure = Figure(figsize=(10, 10), dpi=DPI)
@@ -187,14 +129,14 @@ def draw_plots_2(trim_fix: dict, cali_trim_fix: dict, stat_string:str, image_pat
     axes = figure.subplots(ncols=2, nrows=2)
     axes[0][0].set_title('Original')
     axes[0][0].imshow(img, zorder=1)
-    
+
     axes[0][1].set_title('Fixation Heatmap')
     axes[0][1].hist2d(trim_fix['x'], trim_fix['y'], weights=trim_fix['dur'], bins=BINS,
                       range=RANGE_PIC, alpha=0.8, zorder=2, cmin=0.0001)
     axes[0][1].invert_yaxis()
     axes[0][1].imshow(img, zorder=1)
     axes[1][0].set_title('Stats')
-    axes[1][0].text(0.01,0.99, stat_string, horizontalalignment='left',
+    axes[1][0].text(0.01, 0.99, stat_string, horizontalalignment='left',
                     verticalalignment='top', multialignment='left')
     if cali_trim_fix is not None:
         axes[1][1].set_title('Numbered Fixations')
@@ -216,40 +158,7 @@ def draw_plots_2(trim_fix: dict, cali_trim_fix: dict, stat_string:str, image_pat
     figure.clf()
 
 
-def create_plots(ids=(3, 14), dats=DATASETS, good_pic=True):
-    for i in range(ids[0], ids[1]):
-        for d in dats:
-            print('id: {}, dat: {}'.format(i, d))
-            ici = get_file_paths(PATH.format(i, d))
-            for im, cali, im_path in ici:
-                impk = pd.read_pickle(im)
-                im_cali = None
-                if cali:
-                    im_cali = offset_calibration(impk.dropna(), pd.read_pickle(cali))
-                else:
-                    im_cali = offset_calibration_default(impk.dropna())
-                cali_trim = trim_image(im_cali)
-
-                trim = trim_image(impk.dropna().drop('times', axis=1).values)
-                _, fixations = detectors.fixation_detection_dd(impk['x'].values, impk['y'].values, impk['times'].values,
-                                                            maxdist=42, mindur=100)
-                _, saccades = detectors.saccade_detection(impk['x'].values, impk['y'].values, impk['times'].values,
-                                                          minlen=20, maxvel=MAX_VEL)
-                trim_fix = trim_fixations(gazeplotter.parse_fixations(fixations))
-                trim_sac = trim_saccades(saccades)
-
-                path = 'plots/{}/{}/'.format(i, d)
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                good_path = 'plots/{}/'.format(d)
-                if not os.path.exists(good_path):
-                    os.makedirs(good_path)
-                save_path = path + im.rpartition('/')[-1][:-4] + '.png'
-                good_path += str(i) + '_' + im.rpartition('/')[-1][:-4] + '.png'
-                draw_plots(trim, cali_trim, im_path, save_path, trim_fix, trim_sac, good_path)
-
-
-def create_plots_2(ids=(3, 15), dats=DATASETS):
+def create_plots(ids=(3, 15), dats=DATASETS):
     image_df = Prepare.load_images_dataframe()
     for i in range(ids[0], ids[1]):
         for d in dats:
@@ -258,14 +167,13 @@ def create_plots_2(ids=(3, 15), dats=DATASETS):
             for im, cali, im_path in ici:
                 image_name = re.findall(IMAGE_NAME_REGEX, im_path)[0]
                 impk = pd.read_pickle(im)
-                im_cali = None
                 if cali:
                     im_cali = offset_calibration(impk.dropna(), pd.read_pickle(cali))
                 else:
                     im_cali = offset_calibration_default(impk.dropna())
 
                 _, fixations = detectors.fixation_detection_dd(impk['x'].values, impk['y'].values, impk['times'].values,
-                                                            maxdist=42, mindur=100)
+                                                               maxdist=42, mindur=100)
                 trim_fix = trim_fixations(gazeplotter.parse_fixations(fixations))
                 _, fixations = detectors.fixation_detection_dd(im_cali[:, 0], im_cali[:, 1], im_cali[:, 2],
                                                                maxdist=42, mindur=100)
@@ -282,12 +190,11 @@ def create_plots_2(ids=(3, 15), dats=DATASETS):
                     os.makedirs(good_path)
                 good_path += str(i) + '_' + im.rpartition('/')[-1][:-4] + '.png'
                 if len(trim_fix['x']) > 0 and len(cali_trim_fix['x']) > 0:
-                    draw_plots_2(trim_fix, cali_trim_fix, stat_string, im_path, good_path)
+                    draw_plots(trim_fix, cali_trim_fix, stat_string, im_path, good_path)
 
 
 def find_middle_lines(ids=(3, 15), dats=DATASETS):
     image_df = Prepare.load_images_dataframe()
-    cont = "c"
     for i in range(ids[0], ids[1]):
         for d in dats:
             print('id: {}, dat: {}'.format(i, d))
@@ -407,7 +314,7 @@ def fix_trial_number(ids=(3, 15), dats=DATASETS):
             ici = get_file_paths(PATH.format(i, d))
             for im, cali, im_path in ici:
                 image_name = re.findall(IMAGE_NAME_REGEX, im_path)[0]
-                trial_number = re.findall("(\d{1,2})_", im_path)[0]
+                trial_number = re.findall(r"(\d{1,2})_", im_path)[0]
                 image_df.at[(i, d, image_name), 'trial'] = str(int(trial_number) + 1)
 
     image_df.to_csv(Prepare.IMAGE_CSV_PATH)
